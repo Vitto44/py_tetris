66dfd329919b13e084988a1f3afc32ad34bdd135
Viktor Hajdu <vi.h.seo@gmail.com>
2023-08-24 14:56:11 +0200

    add basic screen

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import pygame, sys
+
+pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((300, 600))
+
+# Title and Icon
+pygame.display.set_caption("Tetris")
+clock = pygame.time.Clock()
+
+
+# Game Loop
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    clock.tick(60)
+    # RGB = Red, Green, Blue
+    screen.fill((10, 10, 10))
+
+    pygame.display.update()
